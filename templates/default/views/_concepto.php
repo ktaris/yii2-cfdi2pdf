8c02fd549e19a606d3a2d1eadd64f77484860b5e
Carlos Ramos <carlos@ramoscarlos.com>
2018-04-27 00:44:39 -0600

    Agregado descuento a lista de cosas a ser impresas por concepto

--- templates/default/views/_concepto.php
+++ templates/default/views/_concepto.php
@@ -11,6 +11,15 @@ $clase = ($i === 0) ? 'primero' : 'no-primero';
     <td class="text-right"><?= $concepto->ValorUnitario ?></td>
     <td class="text-right"><?= $concepto->Importe ?></td>
 </tr>
+<?php if ($concepto->Descuento > 0) : ?>
+<tr class="descuento">
+<td colspan="7" class="text-right">
+<strong>Descuento</strong>
+<strong>[</strong><?= $concepto->Descuento ?><strong>]</strong>
+</td>
+</tr>
+<?php endif; ?>
+
 <?php if ($concepto->tieneImpuestos) : ?>
     <?= $this->render('_impuestos', [
         'concepto' => $concepto,
